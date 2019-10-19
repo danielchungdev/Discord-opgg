5ef353ae37bb83e0fab1dc0145472ebab6f5a983
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-10-19 03:16:15 -0400

    First Functions
    ** getSummonerName and getSummonerID **

--- discordgg.py
+++ discordgg.py
@@ -1,20 +1,37 @@
 import discord
-
-import discord
+import requests
 
 client = discord.Client()
-token = ""
+discordToken = "NjE4ODM2MDE0OTQ5MjAzOTg3.Xaqzyw.osNryIZLPM6PSxp96yYu_fpa_4w"
+riotApiKey = "RGAPI-7c02fddb-2cf7-45d2-9e5c-58dd7cfd5704"
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    """
+    Checks to see if the bot is activated ! is going to be our key
+    character. When the bot recognizes this it will execute code.
+    """
+    if message.content[0] == "!":
+        summonerName = getSummonerName(message.content)
+        print(getSummonerID(summonerName))
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
-client.run('your token here')
+def getSummonerName(commandMessage):
+    return commandMessage[1:]
+
+
+def getSummonerID(summonerName):
+    URL = "https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + riotApiKey
+    response = requests.get(url=URL)
+    data = response.json()
+    summonerID = data['id']
+    return summonerID
+
+
+client.run(discordToken)
