384d27149ef2b076ae8306f23159a626fc74329e
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-10-19 03:50:45 -0400

    Basics Finished
    ** Final functiona functions Finished **

--- discordgg.py
+++ discordgg.py
@@ -19,8 +19,9 @@ async def on_message(message):
     """
     if message.content[0] == "!":
         summonerName = getSummonerName(message.content)
-        print(getSummonerID(summonerName))
-
+        summonerID = getSummonerID(summonerName)
+        print(getSummonerRank(summonerID))
+        await message.channel.send(getSummonerRank(summonerID))
 
 def getSummonerName(commandMessage):
     return commandMessage[1:]
@@ -33,5 +34,31 @@ def getSummonerID(summonerName):
     summonerID = data['id']
     return summonerID
 
+def getSummonerRank(summonerID):
+    URL = "https://la1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerID + "?api_key=" + riotApiKey
+    response = requests.get(url=URL)
+    data = response.json()
+    index = getQueueType(data)
+    tier = data[index]['tier']
+    rank = data[index]['rank']
+    wins = data[index]['wins']
+    losses = data[index]['losses']
+    lp = str(data[index]['leaguePoints'])
+    winRatio = getWinRatio(wins, losses)
+    fullRank = "  " +tier + " " + rank + " " + lp + "lp.\nW/L: " + str(wins) +"/"+ str(losses) + " WR:" + str(winRatio) + "%."
+    return fullRank
+
+def getQueueType(data):
+    if data[0]["queueType"] == "RANKED_SOLO_5x5":
+        return 0
+    elif data[1]["queueType"] == "RANKED_SOLO_5x5":
+        return 1
+    elif data[2]["queueType"] == "RANKED_SOLO_5x5":
+        return 2
+
+def getWinRatio(wins, losses):
+    x = wins + losses
+    y = wins * 100
+    return round(y / x)
 
 client.run(discordToken)
