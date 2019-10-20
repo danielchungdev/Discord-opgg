c11fbd5b98d10d0cc3b14cb4ebad1347af4ebad4
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-10-20 01:02:15 -0400

    Commenting discordgg.py
    ** Comments added to all functions **

--- discordgg.py
+++ discordgg.py
@@ -1,6 +1,6 @@
 import discord
 import requests
-
+#CHANGE DISCORD TOKEN EVERYTIME
 client = discord.Client()
 discordToken = "NjE4ODM2MDE0OTQ5MjAzOTg3.Xatd4Q.YlQgLlulO6zRgi7ehFFEA6wrxCU"
 riotApiKey = "RGAPI-7c02fddb-2cf7-45d2-9e5c-58dd7cfd5704"
@@ -11,12 +11,14 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
+"""
+Main method checks if the user's message has the keyletter "!"
+if it has then it proceeds to check for the summonername and 
+retrieve information.
+"""
+
 @client.event
 async def on_message(message):
-    """
-    Checks to see if the bot is activated ! is going to be our key
-    character. When the bot recognizes this it will execute code.
-    """
     if message.content[0] == "!":
         summonerName = getSummonerName(message.content)
         summonerID = getSummonerID(summonerName)
@@ -26,10 +28,23 @@ async def on_message(message):
             print(getSummonerRank(summonerID, summonerName))
             await message.channel.send(getSummonerRank(summonerID, summonerName))
 
+"""
+Param: userInput. 
+This functions gets the summonerName from
+the message input. 
+Return: (str) summonerName. (Basically a strip.)
+"""
 def getSummonerName(commandMessage):
     return commandMessage[1:]
 
-
+"""
+Param: summonerName (from user input)
+This function gets the summonerID (different from summoner name)
+this is used for future use. Here also happens the check for error
+404 meaning that the summoner doesn't exist and therefore doing 
+error handling.
+Return: (boolean) if Error, (str) summonerID 
+"""
 def getSummonerID(summonerName):
     URL = "https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + riotApiKey
     response = requests.get(url=URL)
@@ -42,6 +57,13 @@ def getSummonerID(summonerName):
         summonerID = data['id']
         return summonerID
 
+"""
+Param: summonerID (getSummonerID), summoner (summonerName from user input).
+This function is the one that is responsible for the main 
+method of the application. It will format and return the 
+rank information of the summoner name to be displayed. 
+Returns: (str) User's ranked information or No ranked games.  
+"""
 def getSummonerRank(summonerID, summoner):
     URL = "https://la1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerID + "?api_key=" + riotApiKey
     response = requests.get(url=URL)
@@ -59,6 +81,13 @@ def getSummonerRank(summonerID, summoner):
     else:
         return "Summoner has no Ranked games"
 
+"""
+Param: Data (retrieved from the json in getSummonerRank()).
+So data from the Riot Games API comes in randomly. Meaning that the 
+queuetype is not static and is always changing. For it this method
+checks in what position/index "Ranked Solo 5v5" is in and returns it.
+Returns: (int) Position where "Ranked Solo 5v5" is in.
+"""
 def getQueueType(data):
     if len(data) == 0:
         return False;
@@ -69,6 +98,11 @@ def getQueueType(data):
     elif data[2]["queueType"] == "RANKED_SOLO_5x5":
         return 2
 
+"""
+Params: Users Wins, Users Losses.
+This function is a basic rule of thirds to get the users Win Ratio.
+Returns: (int) Win Ratio.
+"""
 def getWinRatio(wins, losses):
     x = wins + losses
     y = wins * 100
